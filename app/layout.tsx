10c37b0b3a98f81230654ff3e416158e25ebeabf
DanielEgiidio <danielegidioti@gmail.com>
2023-11-01 17:50:28 -0300

    feat: setup da auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { ClerkProvider } from '@clerk/nextjs';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +16,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <div className="flex min-h-screen w-full flex-col items-center dark:bg-black">{children}</div></body>
+      </html>
+    </ClerkProvider>
   )
 }
